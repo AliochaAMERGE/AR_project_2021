d58af8ff43a3b1e7020bfd47a595631bc28f0493
aliocha <aliocha.amerge@hotmail.fr>
2021-04-04 20:24:25 +0200

    f en plus

--- key_search.c
+++ key_search.c
@@ -2,19 +2,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define TAGINIT 0
-#define NB_SITE 6
+#define N 11  // nombre de sites (processus) en comptant le processus initiateur
+#define M 6   // nombre de fingers
 
-// TODO
-/*  !!!!! valeur a initialiser !!!!!*/
-#define NB_SITE //nombre de sites (processus)
-#define M //nombre de fingers
+// ! il faut que N > M
 
 /*LES TAGS */
-#define TAG_INIT
-//... d'autres TAG maybe 
-#define TAG_TERMINAISON
+#define TAG_INIT 0
+//... d'autres TAG maybe
+#define TAG_TERMINAISON 1
 
+/**********************Fonction d'appartenance à [a,b[************************/
+/******************************************************************************
+ *               app vérifie si la clé k appartient à                         *
+ *               l'intervalle [a,b[                                           *
+ *****************************************************************************/
+
+int app(int k, int a, int b)
+{
+  if (a < b) return ((k >= a) && (k < b));
+  return (((k >= 0) && (k < b)) || ((k >= a) && (k < N)));
+}
 
 /******************************Fonctions Aléatoires****************************/
 
@@ -24,12 +32,52 @@
  ******************************************************************************/
 
 // p : rank
-void f(int p) {}
+// id_already_used : contient les idChord pour les p-1 ranks, p : rank
+int f(int** id_already_used, int p)
+{
+  srand(time(NULL));
+  int alea_chord;
+  // on a 2^M-1 valeurs possibles
+  int i = 0;
+  do
+  {
+    alea_chord = (rand() % pow((M, 2) - 1));  // retire une valeur aléatoire
+    if (id_already_used[i] == alea_chord)
+    {
+      i = 0;  // repart du début
+    }
+    else
+    {
+      i++;
+    }
+  } while (i < p);
+
+  id_already_used[p] = alea_chord;
+
+  return alea_chord;
+}
 /******************************************************************************
  *            g retourne aléatoirement un identifiant unique pour une donnée  *
  *            (clés) parmis l'ensemble des données du système                 *
  ******************************************************************************/
 void g(void) {}
+
+/* trier un tableau */
+
+int compare(const void* a, const void* b)
+{
+  int int_a = *((int*)a);
+  int int_b = *((int*)b);
+
+  if (int_a == int_b)
+    return 0;
+  else if (int_a < int_b)
+    return -1;
+  else
+    return 1;
+}
+
+// qsort( array, element_count, element_size, *compare_function )
 
 /*************Initialisation de la DHT par le processus simulateur*************/
 
@@ -41,19 +89,48 @@ void simulateur(void)
    * la premiere case de ces derniers contiendra -1 pour le rank 0 car c'est *
    * lui qui est le processus simulateur                                     *
    ***************************************************************************/
-  int CHORD[NB_SITE][2]; // contient un ensemble de couple (MPI rank, id chord)
-  int fingers[NB_SITE][M]; // contient des ensemble de M fingers pour chaque site
-  int succ[NB_SITE]; // contient l'identifiant CHORD du successeur succ[i]
+  int CHORD[N - 1][2];    // contient un ensemble de couple (MPI rank, id chord)
+  int fingers[N - 1][M];  // contient des ensemble de M fingers pour chaque site
+  int succ[N - 1];        // contient l'identifiant CHORD du successeur succ[i]
 
   /*initialisation des identifiants CHORD, soit le tableau CHORD */
 
+  int* id_already_used;
+
+  for (size_t p = 0; p < N - 1; p++)
+  {
+    f(*id_already_used, p);
+  }
+  qsort(*id_already_used, N - 1, sizeof(int), compare);
+
+  for (size_t p = 0; p < N - 1; p++)
+  {
+    CHORD[p][0] = p + 1;               // MPI_Rank
+    CHORD[p][1] = id_already_used[p];  // ID_Chord
+  }
+
   /*initialisation des fingers, soit le tableau fingers*/
 
-  /*initialisation du successeur de chacun des sites*/
+  for (size_t p = 0; p < N - 1; p++)
+  {
+    /* initialisation du successeur du site p */
+    fingers[p][0] = id_already_used[(p + 1) % N];
 
-  // NE PAS OUBLIER DE METTRE LES PAIRS DANS L'ORDRE CROISANT ==>> ANNEAU !!!!
-  // EN FONCTION DES ID CHORD ET NON PAS RANK
-  
+    // chaque process se voit assigner M fingers (f0 = succ)
+    // tel que les fingers sont compris entre succ et process p + M/2 (moitié du
+    // tableau)
+    for (size_t f = 1; f < M; f++)
+    {
+      // TODO check if it works
+      int fing =
+          ((p + 1) + rand() % ((p + 1) + N / 2)) % N;  // ! vraiment pas sur
+      fingers[p][f] = id_already_used[fing];
+    }
+  }
+
+  // NE PAS OUBLIER DE METTRE LES PAIRS DANS L'ORDRE CROISANT ==>> ANNEAU !!!!  ~fait
+  // EN FONCTION DES ID CHORD ET NON PAS RANK                                   ~fait
+
   /*envoi à chacun des pairs du système leurs variables*/
   //===>>>> MPI_Send à faire
   // tag à créer ???? a voir
@@ -71,16 +148,18 @@ void simulateur(void)
 /*
 p4 recoit de P0
 {-1, id1, id2, id3, -1, -1, id6, id7, -1}
-id1, id2, id3, id6, id7 <- pas ça 
+id1, id2, id3, id6, id7 <- pas ça
 id4
 
-CHORD => contient tous les couples (rank, id) de chaque pair de anneau ou id correspond à l'id du pair 
+CHORD => contient tous les couples (rank, id) de chaque pair de anneau ou id
+correspond à l'id du pair
 
 fingers contient des sous tableaux de finger pour chaque pair
 
-pour l'envoi des fingers il faudra créer un tableau chord_tmp dans lequel on mettre SEULEMENT les couples 
-correspond au id_chord des pairs qui sont dans le sous tableau de finger du proc i
-et donc chaque proc i aura l'information seulement des ses finger pair id et son rang mpi
+pour l'envoi des fingers il faudra créer un tableau chord_tmp dans lequel on
+mettre SEULEMENT les couples correspond au id_chord des pairs qui sont dans le
+sous tableau de finger du proc i et donc chaque proc i aura l'information
+seulement des ses finger pair id et son rang mpi
 
 p34, p44, p55
 
@@ -96,7 +175,7 @@ int main(int argc, char* argv[])
   MPI_Init(&argc, &argv);
   MPI_Comm_size(MPI_COMM_WORLD, &nb_proc);
 
-  if (nb_proc != NB_SITE + 1)
+  if (nb_proc != N + 1)
   {
     printf("Nombre de processus incorrect !\n");
     MPI_Finalize();
